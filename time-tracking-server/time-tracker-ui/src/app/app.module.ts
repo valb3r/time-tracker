68e792b42991c1f8bd4f4641d7bfc0e90edf7e1f
Valentyn Berezin <valb3r.ua@gmail.com>
2020-01-06 15:27:19 +0200

    Initial date picker component

---
+++ time-tracking-server/time-tracker-ui/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/moment';
+import * as moment from 'moment';
+
+export function momentAdapterFactory() {
+  return adapterFactory(moment);
+}
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CalendarModule.forRoot({ provide: DateAdapter, useFactory: momentAdapterFactory })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
