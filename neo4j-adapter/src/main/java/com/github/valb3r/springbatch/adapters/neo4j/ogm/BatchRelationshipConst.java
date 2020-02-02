59fbba92b82bf01dda5b913e851fc16893341d85
Valentyn Berezin <valb3r.ua@gmail.com>
2020-02-02 23:13:50 +0200

    TTK-5. Using published repository for neo4j adapter

--- neo4j-adapter/src/main/java/com/github/valb3r/springbatch/adapters/neo4j/ogm/BatchRelationshipConst.java
+++
@@ -1,9 +0,0 @@
-package com.github.valb3r.springbatch.adapters.neo4j.ogm;
-
-import lombok.experimental.UtilityClass;
-
-@UtilityClass
-public class BatchRelationshipConst {
-
-    public static final String PARENT = "PARENT";
-}
