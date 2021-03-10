c38d89f67b355b1e76bb43ae8008d199560d5199
Valentyn Berezin <vbe@adorsys.com.ua>
2021-03-10 20:47:51 +0200

    Added multi-CORS support

--- time-tracking-server/src/main/java/ua/timetracker/timetrackingserver/config/jwtsecurity/CorsWebFluxConfigurer.java
+++ time-tracking-server/src/main/java/ua/timetracker/timetrackingserver/config/jwtsecurity/CorsWebFluxConfigurer.java
@@ -1,19 +1,38 @@
 package ua.timetracker.timetrackingserver.config.jwtsecurity;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+
 @Slf4j
 @Configuration
+@RequiredArgsConstructor
 public class CorsWebFluxConfigurer implements WebFluxConfigurer {
+
+    private final CorsConfig corsConfig;
 
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
-            .allowedOrigins("http://localhost:6500")
-            .allowCredentials(true)
-            .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+                .allowedOrigins(corsConfig.getOrigins())
+                .allowCredentials(true)
+                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+    }
+
+    @Valid
+    @Data
+    @Configuration
+    @ConfigurationProperties(prefix = "cors")
+    public static class CorsConfig {
+
+        @NotEmpty
+        private String[] origins;
     }
 }
