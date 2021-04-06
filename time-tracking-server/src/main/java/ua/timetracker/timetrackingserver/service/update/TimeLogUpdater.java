c2762f8872d0f9ac380a1e302f4bb7298809f3bd
Valentyn Berezin <vbe@adorsys.com.ua>
2021-04-06 21:04:23 +0300

    Removed 'Serializable'

--- time-tracking-server/src/main/java/ua/timetracker/timetrackingserver/service/update/TimeLogUpdater.java
+++ time-tracking-server/src/main/java/ua/timetracker/timetrackingserver/service/update/TimeLogUpdater.java
@@ -2,7 +2,6 @@ package ua.timetracker.timetrackingserver.service.update;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Mono;
 import ua.timetracker.shared.persistence.entity.projects.TimeLog;
@@ -20,7 +19,8 @@ public class TimeLogUpdater {
 
     private final TimeLogsRepository timeLogs;
 
-    @Transactional(value = REACTIVE_TX_MANAGER, isolation = Isolation.SERIALIZABLE)
+    // TODO - need to obtain locks to increment properly in concurrent setting. Serializable is not supported
+    @Transactional(REACTIVE_TX_MANAGER)
     public Mono<TimeLogDto> increment(long userId, long id, Duration duration) {
         return timeLogs.findById(id)
                 .flatMap(it -> {
